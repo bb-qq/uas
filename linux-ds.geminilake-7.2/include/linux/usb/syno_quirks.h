7160fe983a5eae19f37eb4b3030ae8da38b3c2a8
bb-qq <bb-qq@users.noreply.github.com>
2023-11-19 18:12:40 +0900

    Added sources for ds.geminilake-7.2.

---
+++ linux-ds.geminilake-7.2/include/linux/usb/syno_quirks.h
@@ -0,0 +1,28 @@
+#ifndef __LINUX_SYNO_USB_QUIRKS_H
+#define __LINUX_SYNO_USB_QUIRKS_H
+
+/*
+ * Description:
+ *    List all SYNO USB QUIRKS
+ *
+ *
+ *   UPS_DISCONNECT_FILTER is used to avoid disconnection from the UPS by
+ *   deferring a disconnect for a period of time, and then if the UPS re-
+ *   connects, we ignore the last disconnection, otherwise disconnects it.
+ *
+ *   LIMITED_UPS_DISCONNECT_FILTERING is an extension of UPS_DISCONNECT_FILTER
+ *   it's used to limit the number of disconnect filtering in a period of time.
+ *   For the buggy UPS which disconnects very frequently before a UPS driver (
+ *   usually in userspace) links the UPS. Because the condition that the buggy
+ *   UPS isn't stable initally and UPS driver can't also link it before the
+ *   driver stops trying, so we should actually disconnect and re-connect to
+ *   notify and restart the UPS driver
+ */
+
+#define SYNO_USB_QUIRK_UPS_DISCONNECT_FILTER				0x00000001
+#define SYNO_USB_QUIRK_LIMITED_UPS_DISCONNECT_FILTERING		0x00000002
+#define SYNO_USB_QUIRK_SYNCHRONIZE_CACHE_FILTER				0x00000010
+#define SYNO_USB_QUIRK_HC_MORE_TRANSACTION_TRIES			0x00000020
+
+#endif /* __LINUX_SYNO_USB_QUIRKS_H */
+
